957f3e060cf83a8ba435d7253d6296c357f38c82
MartinKratkyProject <martin.kratky.job@gmail.com>
2024-04-19 18:51:47 +0200

    Update myfirstprojectEDITED_copy_20240412113451.ino

--- myfirstprojectEDITED_copy_20240412113451/myfirstprojectEDITED_copy_20240412113451.ino
+++ myfirstprojectEDITED_copy_20240412113451/myfirstprojectEDITED_copy_20240412113451.ino
@@ -4,8 +4,8 @@
 #include <WiFiUdp.h>
 #include <Servo.h>
 
-const int servoPin = 1; // Pin for servo control
-Servo servo360; // Servo object
+const int servoPin = 1;
+Servo servo360;
 
 #define FIREBASE_HOST "myfirstproject-fa65c-default-rtdb.europe-west1.firebasedatabase.app"
 #define FIREBASE_AUTH "lQL8ehEa4s5sFAzNqaFI1VIE8OfwOm2oU9fGIoiF"
@@ -78,52 +78,43 @@ void loop() {
   distance = (duration*.0343)/2;
   Serial.print("Distance: ");
   Serial.println(distance);
-  delay(500);
+  delay(300);
 
   if (distance < 10 & distance > 3) {
-    //delay(2000);
-    servo360.write(90); // Stop the servo
-    getColorAndSendData(); // Get color data and send to Firebase
-  } else {
-    servo360.write(80); // Rotate the servo
-  }
-}
+    delay(1000);
+    servo360.write(90);
 
+    //red color
+    digitalWrite(A2, LOW);
+    digitalWrite(A3, LOW);
+    frequencyRED = pulseIn(sensor, LOW);
+    frequencyRED = map(frequencyRED, 25, 72, 255, 0);
+    Serial.print("R = ");
+    Serial.print(frequencyRED);
+    Serial.print("  ");
+    delay(100);
 
+    //green color
+    digitalWrite(A2, HIGH);
+    digitalWrite(A3, HIGH);
+    frequencyGREEN = pulseIn(sensor, LOW);
+    frequencyGREEN = map(frequencyGREEN, 30, 90, 255, 0);
+    Serial.print("G = ");
+    Serial.print(frequencyGREEN);
+    Serial.print("  ");
+    delay(100);
 
+    //blue color
+    digitalWrite(A2, LOW);
+    digitalWrite(A3, HIGH);
+    frequencyBLUE = pulseIn(sensor, LOW);
+    frequencyBLUE = map(frequencyBLUE, 25, 70, 255, 0);
+    Serial.print("B= ");
+    Serial.print(frequencyBLUE);
+    Serial.println("  ");
+    delay(100);
 
-void getColorAndSendData() {
-  //red color
-  digitalWrite(A2, LOW);
-  digitalWrite(A3, LOW);
-  frequencyRED = pulseIn(sensor, LOW);
-  frequencyRED = map(frequencyRED, 25, 72, 255, 0);
-  Serial.print("R = ");
-  Serial.print(frequencyRED);
-  Serial.print("  ");
-  delay(100);
-
-  //green color
-  digitalWrite(A2, HIGH);
-  digitalWrite(A3, HIGH);
-  frequencyGREEN = pulseIn(sensor, LOW);
-  frequencyGREEN = map(frequencyGREEN, 30, 90, 255, 0);
-  Serial.print("G = ");
-  Serial.print(frequencyGREEN);
-  Serial.print("  ");
-  delay(100);
-
-  //blue color
-  digitalWrite(A2, LOW);
-  digitalWrite(A3, HIGH);
-  frequencyBLUE = pulseIn(sensor, LOW);
-  frequencyBLUE = map(frequencyBLUE, 25, 70, 255, 0);
-  Serial.print("B= ");
-  Serial.print(frequencyBLUE);
-  Serial.println("  ");
-  delay(100);
-
-  if (frequencyBLUE > -200 && frequencyGREEN > -200 && frequencyRED > -240) {
+  if (frequencyBLUE > 0 && frequencyGREEN > 0 && frequencyRED > 0) {
     //biela high
     myservo.write(180); 
     color = "white";
@@ -144,10 +135,11 @@ void getColorAndSendData() {
       color = "green";
     }
   }
+
   timeClient.update();
   unsigned long epochTime = timeClient.getEpochTime();
 
-  unsigned long epochDay = epochTime / 86400; // 86400 seconds in a day
+  unsigned long epochDay = epochTime / 86400;
   int year = 1970;
   int month, day, hour, minute, second;
   while (epochDay >= (365 + ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))) {
@@ -178,4 +170,7 @@ void getColorAndSendData() {
   Serial.println(color + " " + timestamp);
   servo360.write(80);
   delay(2000);
+  } else {
+    servo360.write(80);
+  }
 }
